404220a2b52b0a3f7f50802cc59b3ec62f7781b8
Jarkko Inki <jarkko.inki@maxiv.lu.se>
2018-01-24 13:13:37 +0100

    adding color to the values given by the crosshair

--- src/ui_icepapcms/ui_dialogpreferences.py
+++ src/ui_icepapcms/ui_dialogpreferences.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'dialogpreferences.ui'
 #
-# Created: Wed Jan 24 10:10:50 2018
+# Created: Wed Jan 24 10:23:36 2018
 #      by: PyQt4 UI code generator 4.10.1
 #
 # WARNING! All changes made in this file will be lost!
