29709fc51054ba1820dd1ad84e2770e57e2fb3ea
Antoine Dupre <antoine.dupre@maxiv.lu.se>
2018-02-15 17:51:09 +0100

    Bump version 2.2

--- src/setup.py
+++ src/setup.py
@@ -8,7 +8,7 @@ if os.name == 'nt': #sys.platform == 'win32':
     import py2exe
 
 setup(name = "icepapcms",
-      version = "2.1",
+      version = "2.2",
       description = "Icepap Configuration Management System and Test Tool",
       author = "Guifre Cuni",
       author_email = "gcuni@cells.es",
